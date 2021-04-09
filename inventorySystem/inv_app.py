fa4554db886ab4cabdee27a1d6046d77595af826
Joel Poirier <devjoelmichael@Joels-Mac-mini.local>
2021-04-09 10:56:36 -0700

    inventory complete, buy working progress

--- inventorySystem/inv_app.py
+++ inventorySystem/inv_app.py
@@ -1,3 +1,5 @@
+# Make sure you have a clear readable Menu
+
 print('''
 --------------------------
 Welcome to ABC Electronics
@@ -6,3 +8,33 @@ Welcome to ABC Electronics
 2. Buy Items
 3. Exit
 ''')
+
+# Make sure the choices are numerical and in order when customer is inputing their choice
+
+choice = int(input("Enter Your Option = "))
+
+if choice == 1:
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*")
+    print("Items Available in ABC Electronics")
+    # Make sure the data is opened in read mode and that the proper txt file is linked
+    data = open("inventory.txt", "r")
+    items = data.readlines()
+    # Make the inventory list readable and user friendly
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*")
+    for item in items:
+        name, price, count = item.split(",")
+        print("{0}\t{1}\t{2}".format(name, price, count))
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+if choice == 2:
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    print("Please Select the Item you want")
+    # Make sure the data is opened in read mode and that the proper txt file is linked
+    data = open("inventory.txt", "r")
+    items = data.readlines()
+    # Make the inventory list readable and user friendly
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    for item in items:
+        name, price, count = item.split(",")
+        print("{0}\t{1}\t{2}".format(name, price, count))
+    print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
